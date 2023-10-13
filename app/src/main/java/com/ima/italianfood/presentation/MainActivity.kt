0d01854dea05bccfb4457e70c3d9b38e99c28efc
fender <avtomax12345@gmail.com>
2023-10-13 10:33:57 +0300

    1) version code 4 added. 2) All libraries updated.

--- app/src/main/java/com/ima/italianfood/presentation/MainActivity.kt
+++ app/src/main/java/com/ima/italianfood/presentation/MainActivity.kt
@@ -11,7 +11,7 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    //@RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
